431606b3770a6acb63c56f047c00923400860115
akash-siripuram <siripuramakash123@gmail.com>
2020-07-29 20:43:17 +0530

    Update tom-my.py

--- tom-my.py
+++ tom-my.py
@@ -2,13 +2,17 @@ import streamlit as st
 import numpy as np
 from sklearn import preprocessing
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+#from sklearn.cluster import KMeans
+#from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 
 
 d=pd.read_excel("Mann.xlsx")
-
+X=d[['A','B','C']]
+y=d['Y']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 st.write("The shape is ",d.shape)
 
 #cl=st.selectbox("Select the number of clusters",[2,3,4,5,6,7,8,9,10])
@@ -17,47 +21,18 @@ b=st.number_input("Enter the Last Price last digit",value=2,step=1)
 c=st.number_input("Enter the Next Period last digit(You want to predict)",value=2,step=1)
 g,r=0,0
 
-
+p=-1
 if a is None or b is None or c is None:
 		st.print("")
 else:
-	c_type=st.selectbox("Cluster Type",["Automatic","Manual"])
-	if c_type=="Automatic":
-		stat=st.selectbox("Select EVEN or ODD 2nd digit ",['EVEN','ODD'])
-		if stat=="EVEN":
-			if st.button("Get Result"):	
-				with st.spinner('In Progress...'):
-					for i in range(2,11):
-						kmeans = KMeans(n_clusters=i, random_state=0).fit(d)
-						a=kmeans.predict([[a,b,c]])
-						if a[0]%2==0:
-							g=g+1
-						else:
-							r=r+1
-					if g>r:
-						st.success("Next is green {}%".format((round(g/11)*100)))
-					else:
-						st.error("Next is Red {}".format(round((r/11)*100)))
-		elif stat=="ODD":
-			if st.button("Get Result"):	
-				with st.spinner('In Progress...'):
-					for i in range(2,11):
-						kmeans = KMeans(n_clusters=i, random_state=0).fit(d)
-						a=kmeans.predict([[a,b,c]])
-						if a[0]%2!=0:
-							g=g+1
-						else:
-							r=r+1
-					if g>r:
-						st.success("Next is green {}%".format((round(g/11)*100)))
-					else:
-						st.error("Next is Red {}".format(round((r/11)*100)))						
-	elif c_type=="Manual":
-		c_size=st.selectbox("Select the cluster size",[2,3,5,7,9])
-		if st.button("Get Manual Cluster"):
-			kmeans = KMeans(n_clusters=c_size, random_state=0).fit(d)
-			a=kmeans.predict([[a,b,c]])
-			if a[0]%2==0:
-				st.success("Next is green {}".format(g))
-			else:
-				st.error("Next is Red {}".format(r))
+	s_type=st.selectbox("Type",["Classification","Clustering"])
+	if s_type =="Classification":
+		if st.button("Classify"):
+			with st.spinner('In Progress...'):
+				clf = svm.SVC()
+				clf.fit(X_train,y_train)
+				p=clf.predict([[a,b,c]])
+				if p==0:
+					st.error("Next is RED")
+				elif p==1:
+					st.success("Next is GREEN")
