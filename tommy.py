fce01b3c3948eedfb7e474f9f7d6dd3681feaad3
akash-siripuram <siripuramakash123@gmail.com>
2020-08-02 19:01:06 +0530

    Add files via upload

---
+++ tommy.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
+#from sklearn.cluster import KMeans
+#from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import sklearn
+
+
+
+s_type=st.selectbox("Type",["Parity","Sapre","Bcone","Emerd"])
+#st.write("The shape is ",d.shape)
+#cl=st.selectbox("Select the number of clusters",[2,3,4,5,6,7,8,9,10])
+a=st.number_input("Enter the Last Period last digit",value=2,step=1)
+b=st.number_input("Enter the Last Price last digit",value=2,step=1)
+#c=st.number_input("Enter the Next Period last digit(You want to predict)",value=2,step=1)
+g,r=0,0
+#st.write(int(bin(a)[2:])%2)
+p=-1
+j=1
+if a is None or b is None:
+		st.print("")
+else:
+	if s_type=="Emerd":
+		if st.button("Classify"):
+			with st.spinner('In Progress...'):
+				d=pd.read_excel("lat.xlsx")
+				clf = svm.SVC()
+				#clf = DecisionTreeClassifier(random_state=0)
+				X=d[['A','B','C']]
+				y=d['Y']
+				X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05)
+				#st.write("The shape is ",d.shape)
+				clf.fit(X_train,y_train)
+				st.write("You selected ",s_type)
+				p=clf.predict([[a%2,b%2,(a+1)%2]])
+				if p==0:
+					st.error("Next is RED")
+				elif p==1:
+					st.success("Next is GREEN")
