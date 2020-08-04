ba0422c73b5785870b32c7faee2cdb9736e4ce65
akash-siripuram <siripuramakash123@gmail.com>
2020-08-04 17:24:50 +0530

    Update tommy.py

--- tommy.py
+++ tommy.py
@@ -31,15 +31,15 @@ else:
 				d=pd.read_excel("Emerd_n.xlsx")
 				clf = svm.SVC()
 				#clf = DecisionTreeClassifier(random_state=0)
-				for i in range(0,5):
-					X=d[['A','B','C']]
-					y=d['D']
-					X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.001)
+				X=d[['A','B','C']]
+				y=d['D']
+				X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.001)
 
-					#st.write("The shape is ",d.shape)
+				#st.write("The shape is ",d.shape)
+				for i in range(0,5):
 					clf.fit(X_train,y_train)
 					st.write("You selected ",s_type)
-						
+
 					p=clf.predict([[a,b,a+1]])
 					if p==0:
 						r=r+1
@@ -49,6 +49,6 @@ else:
 						#st.success("Next is GREEN")							
 				if g>r:
 					st.success("Next is GREEN - {} %".format((g/5)*100))
-				else:
+				elif r>g:
 					st.error("Next is RED - {} %".format((r/5)*100))
 
